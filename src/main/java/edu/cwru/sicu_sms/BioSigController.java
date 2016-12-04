57e45ae73a2e95e6d3c4e303e64195ebe99ab097
teddybear02 <theo.frohlich@gmail.com>
2016-12-03 23:07:23 -0500

    Create abstract biosignal controller class

---
+++ src/main/java/edu/cwru/sicu_sms/BioSigController.java
@@ -0,0 +1,22 @@
+/*                                                                      *\
+**                    SICU Stress Measurement System                    **
+**                      Project P04  |  C380 Team A                     **
+**          EBME 380: Biomedical Engineering Design Experience          **
+**                    Case Western Reserve University                   **
+**                          2016 Fall Semester                          **
+\*                                                                      */
+
+package edu.cwru.sicu_sms;
+
+/**
+ * This class is an abstraction for building sub-controller classes to be most likely used by the main {@link Controller} to manage serial connections and other real-time activities pertaining to biomedical signal processing.
+ *
+ * @since December 3, 2016
+ * @author Ted Frohlich <ttf10@case.edu>
+ * @author Abby Walker <amw138@case.edu>
+ */
+abstract class BioSigController {
+    
+    
+    
+}
