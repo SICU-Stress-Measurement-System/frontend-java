19e04df1755f21c9f8f0cb78df7cfee3776bc701
teddybear02 <theo.frohlich@gmail.com>
2016-12-03 19:07:00 -0500

    Define enum 'PortType' for designating ports for specific channels

--- src/main/java/edu/cwru/sicu_sms/Controller.java
+++ src/main/java/edu/cwru/sicu_sms/Controller.java
@@ -52,6 +52,7 @@ public class Controller {
     
     private ObservableList<String> serialPortList;
     private SerialPort eegPort, ekgPort;
+    private enum PortType {EEG_PORT, EKGPORT}
     
     private List<LineChart.Series> eegChannels;
     private LineChart.Series ekgChannel;
