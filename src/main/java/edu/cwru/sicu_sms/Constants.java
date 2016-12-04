c6ba60d93057c424d908309688c2f31acba38096
teddybear02 <theo.frohlich@gmail.com>
2016-12-03 22:28:38 -0500

    Create class for storing constants

---
+++ src/main/java/edu/cwru/sicu_sms/Constants.java
@@ -0,0 +1,22 @@
+/*                                                                      *\
+**                    SICU Stress Measurement System                    **
+**                      Project P04  |  C380 Team A                     **
+**          EBME 380: Biomedical Engineering Design Experience          **
+**                    Case Western Reserve University                   **
+**                          2016 Fall Semester                          **
+\*                                                                      */
+
+package edu.cwru.sicu_sms;
+
+/**
+ * This class is dedicated to storing various constants that might otherwise clutter other source files in this package.
+ *
+ * @since December 3, 2016
+ * @author Ted Frohlich <ttf10@case.edu>
+ * @author Abby Walker <amw138@case.edu>
+ */
+public class Constants {
+    
+    private static final int TIME_WINDOW = 10;
+    
+}
