9d3e113dbc4ab7bc6f0531576bf5fad44b5ddc7d
teddybear02 <theo.frohlich@gmail.com>
2016-12-03 22:40:43 -0500

    Create sub-controllers for managing EEG/EKG portions of the front-end

---
+++ src/main/java/edu/cwru/sicu_sms/EEGController.java
@@ -0,0 +1,22 @@
+/*                                                                      *\
+**                    SICU Stress Measurement System                    **
+**                      Project P04  |  C380 Team A                     **
+**          EBME 380: Biomedical Engineering Design Experience          **
+**                    Case Western Reserve University                   **
+**                          2016 Fall Semester                          **
+\*                                                                      */
+
+package edu.cwru.sicu_sms;
+
+/**
+ * A sub-controller class used by the main {@link Controller} to manage connections and other real-time activities pertaining to the EEG portion of the front-end program.
+ *
+ * @since December 3, 2016
+ * @author Ted Frohlich <ttf10@case.edu>
+ * @author Abby Walker <amw138@case.edu>
+ */
+public class EEGController {
+    
+    
+    
+}
