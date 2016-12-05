d6a640de335d67e6878aaed8c5658779dbc7b5b4
teddybear02 <theo.frohlich@gmail.com>
2016-12-04 19:57:37 -0500

    Delete deprecated inner sub-controller classes

--- src/main/java/edu/cwru/sicu_sms/Controller.java
+++ src/main/java/edu/cwru/sicu_sms/Controller.java
@@ -9,28 +9,15 @@
 package edu.cwru.sicu_sms;
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.*;
 import jssc.SerialPortList;
 
 import java.io.FileWriter;
-import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.Executors;
 
 /**
  * The controller for the front-end program.
@@ -149,122 +136,6 @@ public class Controller {
             disconnect();
             Platform.exit();
         }
-    }
-    
-    
-    /**
-     * A controller for the EEG tab.
-     */
-    @Deprecated
-    private class EEGController {
-    
-        @FXML private LineChart<String, Number>
-                leftRostralChart, rightRostralChart,
-                leftCaudalChart,  rightCaudalChart;
-    
-        private CategoryAxis xAxis;
-        private NumberAxis yAxis;
-        private ObservableList<String> xAxisCategories;
-        
-        private LineChart.Series<String, Number>[] electrodes;
-        
-        private ObservableList<LineChart.Data<String, Number>>
-                leftRostralList, rightRostralList,
-                leftCaudalList,  rightCaudalList;
-    
-        private int lastObservedChangelistSize, changesBeforeUpdate = 10;
-        private Task<Date> chartUpdateTask;
-    
-        private EEGController() {
-            initObservableLists();
-            getObservableLists().forEach(list ->
-                    list.addListener(dataListChangeListener()));
-            
-            initAxes();
-            xAxis.setCategories(xAxisCategories);
-//            xAxis.setAutoRanging(false);
-            
-            //TODO: instantiate and add data series
-            
-            initChartUpdateTask();
-            Executors.newSingleThreadExecutor().submit(chartUpdateTask);
-        }
-        
-        private void initAxes() {
-            xAxis = new CategoryAxis();     yAxis = new NumberAxis();
-            xAxis.setLabel("Time (sec)");   yAxis.setLabel("Relative Amplitude");
-        }
-        
-        private void initChartUpdateTask() {
-            chartUpdateTask = new Task<Date>() {
-                @Override
-                protected Date call() throws Exception {
-                    while (true) {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException ie) {
-                            Thread.currentThread().interrupt();
-                        }
-                        if (isCancelled()) break;
-                        updateValue(new Date());
-                    }
-                    return new Date();
-                }
-            };
-            chartUpdateTask.valueProperty().addListener(new ChangeListener<Date>() {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");  //TODO: eventually just want seconds
-                Random random = new Random();
-                @Override
-                public void changed(ObservableValue<? extends Date> observableDate, Date oldDate, Date newDate) {
-                    String strDate = dateFormat.format(newDate);
-                    xAxisCategories.add(strDate);
-                    getObservableLists().forEach(list ->
-                            list.add(new LineChart.Data(strDate, newDate.getMinutes() + random.nextInt(100500))));
-                }
-            });
-        }
-        
-        private void initObservableLists() {
-            leftRostralList = rightRostralList = leftCaudalList = rightCaudalList
-                    = FXCollections.observableArrayList();
-            xAxisCategories
-                    = FXCollections.observableArrayList();
-        }
-        
-        private List<LineChart<String, Number>> getCharts() {
-            List<LineChart<String, Number>> charts = Collections.emptyList();
-            charts.add(leftRostralChart);   charts.add(rightRostralChart);
-            charts.add(leftCaudalChart);    charts.add(rightCaudalChart);
-            return charts;
-        }
-    
-        private List<ObservableList<LineChart.Data<String, Number>>> getObservableLists() {
-            List<ObservableList<LineChart.Data<String, Number>>> lists = Collections.emptyList();
-            lists.add(leftRostralList);   lists.add(rightRostralList);
-            lists.add(leftCaudalList);    lists.add(rightCaudalList);
-            return lists;
-        }
-        
-        private ListChangeListener<LineChart.Data<String, Number>> dataListChangeListener() {
-            return change -> {
-                if (change.getList().size() - lastObservedChangelistSize > changesBeforeUpdate) {
-                    lastObservedChangelistSize += changesBeforeUpdate;
-                    xAxis.getCategories().remove(0, changesBeforeUpdate);
-                }
-            };
-        }
-        
-    }
-    
-    
-    /**
-     * A controller for the EKG tab.
-     */
-    @Deprecated
-    private class EKGController {
-        
-        //TODO:
-        
     }
     
 }
