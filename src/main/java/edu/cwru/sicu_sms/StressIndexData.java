16cca5afdc7642ca1d094e84b399189306d4702d
amwalker317 <abby.walker317@gmail.com>
2016-12-06 18:08:07 -0500

    Add 'StressIndexData' class

---
+++ src/main/java/edu/cwru/sicu_sms/StressIndexData.java
@@ -0,0 +1,68 @@
+package edu.cwru.sicu_sms;
+
+import java.util.*;
+import javafx.*;
+
+/**
+ * This is a basic List data structure used to represent sets of data used to calculate the patient Stress Index
+ * It is an abstract class, and the child classes are specific types of data used to calculate the stress index,
+ * i.e. EKG, EEG, temporal, or spectral
+ *
+ * @since December 6, 2016
+ * @author Ted Frohlich <ttf10@case.edu>
+ * @author Abby Walker <amw138@case.edu>
+ */
+public abstract class StressIndexData {
+
+    static final int TIMEWINDOW = 300; //seconds of data saved
+    static int MAXSIZE; //Total number of points we want to store/analyze based on sampling frequency and seconds of data saved
+
+    /*The list of data points currently being analyzed to compute the stress index.
+    5 minutes of data are stored at a time*/
+    private ObservableList<T> dataList;
+    private double[] data;
+
+    //The sampling frequency
+    private int Fs;
+
+    //The current stress index component calculated from this data set alone
+    private double stressIndex;
+
+
+    public StressIndexData(ObservableList<T> dataList, int Fs)
+    {
+        this.Fs = Fs;
+        MAXSIZE = Fs*TIMEWINDOW;
+        stressIndex = 0;
+        setList(dataList);
+        setArray();
+    }
+
+    private void setList(ObservableList<T> dataList)
+    {
+        int currSize = dataList.size();
+        int fromIndex = 0;
+        if (currSize > MAXSIZE)
+            fromIndex = currSize-MAXSIZE+1;
+        this.dataList = dataList.subList(fromIndex, currSize);
+
+    }
+
+    /*TODO: figure out how to best add new data points - through observableList or by adding a point directly to array*/
+    public abstract void addDataPoint(/*type to use*/ newPoint);
+
+    public abstract double stressIndex();
+
+    public abstract void setArray();
+
+    public double getSamplingFreq()
+    {
+        return Fs;
+    }
+
+    public double getStressIndex()
+    {
+        return stressIndex;
+    }
+
+}
