bbeb2477ed1dc2c9484bd1cf9abfdac236a4fe25
teddybear02 <theo.frohlich@gmail.com>
2016-12-11 02:33:45 -0500

    Revise style, insert 'todo' comments

--- src/main/java/edu/cwru/sicu_sms/EKGTemporalData.java
+++ src/main/java/edu/cwru/sicu_sms/EKGTemporalData.java
@@ -1,43 +1,59 @@
+/*                                                                      *\
+**                    SICU Stress Measurement System                    **
+**                      Project P04  |  C380 Team A                     **
+**          EBME 380: Biomedical Engineering Design Experience          **
+**                    Case Western Reserve University                   **
+**                          2016 Fall Semester                          **
+\*                                                                      */
+
 package edu.cwru.sicu_sms;
 
 import java.util.*;
 
 /**
- * This class is used to represent a set of temporal (time-domain) EKG data, used to calculate the patient Stress Index
- *  *
+ * This class is used to represent a set of temporal (time-domain) EKG data, used to calculate the patient Stress Index.
+ *
  * @since December 6, 2016
  * @author Ted Frohlich <ttf10@case.edu>
  * @author Abby Walker <amw138@case.edu>
  */
-
 public class EKGTemporalData extends StressIndexData {
 
     public static final int THRESHOLD = 800;
 
-    /*The list of data points currently being analyzed to compute the stress index.
-    5 minutes of data are stored at a time*/
+    /**
+     * Current list of data points, being analyzed to compute the stress index.
+     * <p><i>Note: 5 minutes of data are stored at a time.
+     */
     private ObservableList<T> dataList;
     private double[] data;
 
-    //The sampling frequency
+    /** The sampling frequency, in hertz. */
     private int Fs;
 
-    //The current stress index component calculated from this data set alone
+    /** The current stress index component calculated from this data set alone. */
     private double stressIndex;
 
-
+    /**
+     * Constructs an <code>EKGTemporalData</code> object given a list of data along with its sampling frequency.
+     *
+     * @param dataList the list of data points
+     * @param Fs       the sampling frequency, in hertz
+     */
     public EKGTemporalData(ObservableList<T> dataList, int Fs)
     {
         super(dataList, Fs);
         data = new double[MAXSIZE];
     }
 
+    /** TODO */
     public void setArray()
     {
         data = dataList.toArray();
     }
-
-    public void addDataPoint(/*type to use*/ newPoint)
+    
+    /** TODO */
+    public void addDataPoint(/* TODO: type to use */ newPoint)
     {
         dataList.add(newPoint);
         if (dataList.size() > MAXSIZE)
@@ -47,9 +63,14 @@ public class EKGTemporalData extends StressIndexData {
         setArray();
     }
 
+    /**
+     * Calculates the stress index.
+     *
+     * @return the stress index
+     */
     public double stressIndex()
     {
-        ArrayList<Double> RtagInts = new ArrayList<Double>(MAXSIZE/2);
+        ArrayList<Double> RtagInts = new ArrayList<>(MAXSIZE/2);
         for (int i = 1; i < MAXSIZE; i++)
         {
             time = (1/Fs)*i;
@@ -57,17 +78,17 @@ public class EKGTemporalData extends StressIndexData {
                 RtagInts.add(time);
         }
 
-        for(int i = 1; i < RtagInts.size(); i++)
+        for (int i = 1; i < RtagInts.size(); i++)
         {
             RtagInts.set(i-1, RtagInts.get(i)-RtagInts.get(i-1));
         }
-        RtagInts.remove(RtagInts.size()-1); //final Rtag time will not be replaced by an interval time, so remove it from the list
+        RtagInts.remove(RtagInts.size()-1);  // final Rtag time will not be replaced by an interval time, so remove it from the list
 
         double AAD = calculateAAD(RtagInts);
         return 1/(1+AAD);
-
     }
 
+    /** Calculates... TODO */
     private double calculateAAD(ArrayList RRintervals)
     {
         double intervalSum = 0;
