261177fb52fcb036519d2f4504ac9aac3f93db2b
teddybear02 <theo.frohlich@gmail.com>
2016-12-12 09:24:56 -0500

    Create 'AbstractSerialPort' class in new 'util' package

---
+++ src/main/java/edu/cwru/sicu_sms/util/AbstractSerialPort.java
@@ -0,0 +1,22 @@
+/*                                                                      *\
+**                    SICU Stress Measurement System                    **
+**                      Project P04  |  C380 Team A                     **
+**          EBME 380: Biomedical Engineering Design Experience          **
+**                    Case Western Reserve University                   **
+**                          2016 Fall Semester                          **
+\*                                                                      */
+
+package edu.cwru.sicu_sms.util;
+
+/**
+ * This class is an abstraction of a serial port comprising all the necessary parameters and core functionality needed for the client application.
+ *
+ * @since December 12, 2016
+ * @author Ted Frohlich <ttf10@case.edu>
+ * @author Abby Walker <amw138@case.edu>
+ */
+public abstract class AbstractSerialPort {
+    
+     
+    
+}
