d8a14e54698ec581080f6ef54d34e415bc45d7af
teddybear02 <theo.frohlich@gmail.com>
2016-12-14 04:08:46 -0500

    Create test class for 'PropertyOperator'

---
+++ src/test/java/edu/cwru/sicu_sms/util/PropertyOperatorTest.java
@@ -0,0 +1,46 @@
+/*                                                                      *\
+**                    SICU Stress Measurement System                    **
+**                      Project P04  |  C380 Team A                     **
+**          EBME 380: Biomedical Engineering Design Experience          **
+**                    Case Western Reserve University                   **
+**                          2016 Fall Semester                          **
+\*                                                                      */
+
+package edu.cwru.sicu_sms.util;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for {@link PropertyOperator}.
+ *
+ * @since December 14, 2016
+ * @author Ted Frohlich <ttf10@case.edu>
+ * @author Abby Walker <amw138@case.edu>
+ */
+class PropertyOperatorTest {
+    
+    @BeforeEach
+    void setUp() {
+        
+    }
+    
+    @AfterEach
+    void tearDown() {
+        
+    }
+    
+    @Test
+    void loadProperties() {
+        
+    }
+    
+    @Test
+    void saveProperties() {
+        
+    }
+    
+}
