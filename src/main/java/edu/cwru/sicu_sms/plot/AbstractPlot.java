c6ff4d50e027ef86de6b59a7e5da4bf60d39fe06
teddybear02 <theo.frohlich@gmail.com>
2016-12-14 13:55:29 -0500

    Create 'AbstractPlot' class

---
+++ src/main/java/edu/cwru/sicu_sms/plot/AbstractPlot.java
@@ -0,0 +1,22 @@
+/*                                                                      *\
+**                    SICU Stress Measurement System                    **
+**                      Project P04  |  C380 Team A                     **
+**          EBME 380: Biomedical Engineering Design Experience          **
+**                    Case Western Reserve University                   **
+**                          2016 Fall Semester                          **
+\*                                                                      */
+
+package edu.cwru.sicu_sms.plot;
+
+/**
+ * This class is an abstraction of a channel plot comprising all the necessary parameters and core functionality needed for the client application.
+ *
+ * @since December 14, 2016
+ * @author Ted Frohlich <ttf10@case.edu>
+ * @author Abby Walker <amw138@case.edu>
+ */
+public abstract class AbstractPlot {
+    
+    
+    
+}
